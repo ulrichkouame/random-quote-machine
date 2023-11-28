8b30e936ff8b5762a150219486c4786fe80a71ce
Ulrich Kouame <kouameulrichk@gmail.com>
2023-11-28 16:31:54 +0000

    fix issue : React 18 has known incompatibilities

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Use deprecated rendering method.
+// https://github.com/freeCodeCamp/freeCodeCamp/issues/45922
+
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(<App />,  document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
