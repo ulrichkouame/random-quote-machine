6ce22bf5da8f363204c5bac5f90e6b315f021386
Ulrich Kouame <kouameulrichk@gmail.com>
2023-11-28 16:36:03 +0000

    fix it

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
 // Use deprecated rendering method.
 // https://github.com/freeCodeCamp/freeCodeCamp/issues/45922
 
 // eslint-disable-next-line react/no-deprecated
-ReactDOM.render(<App />,  document.getElementById("root"));
+//ReactDOM.render(<App />,  document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
